4ee5a7d899e9222a488693c2892c7dfc862bbf1f
Alexander Kraiushkin <akraiushkin@gmail.com>
2024-11-08 19:57:00 +0300

    BJS2-27289: Comment analysis v.01.1

--- src/main/java/faang/school/analytics/service/AnalyticsCommentEventService.java
+++ src/main/java/faang/school/analytics/service/AnalyticsCommentEventService.java
@@ -1,14 +1,6 @@
 package faang.school.analytics.service;
 
-import faang.school.analytics.model.dto.AnalyticsEventDto;
-import faang.school.analytics.model.entity.AnalyticsEvent;
-import faang.school.analytics.model.enums.EventType;
-import faang.school.analytics.model.enums.Interval;
 import faang.school.analytics.model.event.CommentEvent;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 public interface AnalyticsCommentEventService {
     void saveCommentEvent(CommentEvent event);
--- src/main/java/faang/school/analytics/service/AnalyticsEventService.java
+++ src/main/java/faang/school/analytics/service/AnalyticsEventService.java
@@ -4,7 +4,6 @@ import faang.school.analytics.model.dto.AnalyticsEventDto;
 import faang.school.analytics.model.entity.AnalyticsEvent;
 import faang.school.analytics.model.enums.EventType;
 import faang.school.analytics.model.enums.Interval;
-import faang.school.analytics.model.event.CommentEvent;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
