b6d5bae634796df5f6514d9dff7685978dc96888
Aleksi Basilashvili <lucheidobra@mail.ru>
2024-12-04 13:21:50 +0300

    Сервис аналитики по заданию BJS2-41472
    Собирает информацию о появлениях профиля пользователя в общем поиске пользователей
    
    Внес изменения + добавил тесты

--- src/main/java/faang/school/analytics/service/AnalyticsEventService.java
+++ src/main/java/faang/school/analytics/service/AnalyticsEventService.java
@@ -2,19 +2,19 @@ package faang.school.analytics.service;
 
 import faang.school.analytics.event.SearchAppearanceEvent;
 import faang.school.analytics.mapper.AnalyticsEventMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class AnalyticsEventService {
 
     private final AnalyticsEventMapper mapper;
 
-    public AnalyticsEventService(AnalyticsEventMapper mapper) {
-        this.mapper = mapper;
-    }
-
     public void processEvent(SearchAppearanceEvent event) {
         String logEntry = mapper.mapToLog(event);
-        System.out.println("Processing event: " + logEntry);
+        log.info("Processing event: " + logEntry);
     }
 }
