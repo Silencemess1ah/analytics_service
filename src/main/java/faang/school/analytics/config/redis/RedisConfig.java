1fd1048fe3776ce47fa2c7c9a5ec823ea98ee53c
VovaBuzivskyi <vov4ikvseti@gmail.com>
2024-12-06 01:45:59 +0700

    added : redis configuration, FollowerEvent,

---
+++ src/main/java/faang/school/analytics/config/redis/RedisConfig.java
@@ -0,0 +1,49 @@
+package faang.school.analytics.config.redis;
+
+import faang.school.analytics.redis.listener.FollowerEventListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.channel.follower-event-channel}")
+    private String followerEventChannel;
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    RedisMessageListenerContainer redisContainer() {
+        RedisMessageListenerContainer container
+                = new RedisMessageListenerContainer();
+        container.setConnectionFactory(jedisConnectionFactory());
+        container.addMessageListener(followerEventListenerAdapter(), topic());
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter followerEventListenerAdapter() {
+        return new MessageListenerAdapter(new FollowerEventListener());
+    }
+
+    @Bean
+    ChannelTopic topic() {
+        return new ChannelTopic(followerEventChannel);
+    }
+}
---
+++ src/main/java/faang/school/analytics/event/FollowerEvent.java
@@ -0,0 +1,25 @@
+package faang.school.analytics.event;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FollowerEvent {
+
+    @NotNull
+    private Long followerId;
+
+    @NotNull
+    private Long followeeId;
+    private LocalDateTime subscriptionTime;
+}
